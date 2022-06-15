c7be13cef770c70914b259ae41dda00cb29a3666
Suraj Kumar <surajsinhaking77@gmail.com>
2022-06-15 20:38:54 +0530

    main

---
+++ String_functions.py
@@ -0,0 +1,46 @@
+# Function of the Strings 
+
+
+#
+# name = "suRaJ Kumar is nice person"
+
+# # upper()
+# name = name2 = name.upper() 
+
+# # lower()
+# name = name2 = name.lower() 
+
+# # Capitalize()
+# name = name2 = name.capitalize()
+
+
+# # title()
+# name = name2 = name.title()
+
+# # Find()
+# name = name2 = name.find("j")
+
+# # index()
+# name = name2 = name.index("")
+
+# print(name2)
+# print(name)
+# print(name.index("nice"))
+
+# h1 = "612736"
+# h2 = "ahdskaj"
+
+# print(h1.isalpha())
+# print(h2.isalpha())
+
+
+# print(h1.isdigit())
+# print(h2.isdigit())
+
+
+
+
+
+
+
+
