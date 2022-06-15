fd86fcaaa34dd4ada548b42d9f776dbbf3bea6b6
Suraj Kumar <surajsinhaking77@gmail.com>
2022-06-15 21:54:52 +0530

    "all add'

--- formateString.py
+++ formateString.py
@@ -6,5 +6,5 @@ favTechLang = "Python"
 print("my name is "+name+" and i am "+str(age)+" years old. My fav language is "+favTechLang)
 print("my name is {} and i am {} years old. My fav languageis {}".format(name,age,favTechLang))
 
-name = "Suraj"
+# name = "Suraj"
 print(f"My name is {name} and i am {age} year old. And My fav Language is {favTechLang}")
